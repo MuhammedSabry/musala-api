bade65359dbaf8d59c593826e44f7bc79b0786c3
MuhammedSabry <muhammed.sabry.refai@gmail.com>
2021-08-09 22:26:29 +0200

    Added crud endpoints to device. Added new exception.

--- server/src/main/java/com/muhammed/sabry/musalaapi/controller/DeviceController.java
+++ server/src/main/java/com/muhammed/sabry/musalaapi/controller/DeviceController.java
@@ -1,5 +1,6 @@
 package com.muhammed.sabry.musalaapi.controller;
 
+import com.muhammed.sabry.musalaapi.exception.InvalidArgumentException;
 import com.muhammed.sabry.musalaapi.exception.NotFoundException;
 import com.muhammed.sabry.musalaapi.mapper.DeviceMapper;
 import com.muhammed.sabry.musalaapi.repository.DeviceEntity;
@@ -37,14 +38,54 @@ public class DeviceController {
 		}
 	}
 	
+	@PutMapping("/device")
+	public void updateDevice(@RequestBody Device device) {
+		Optional<DeviceEntity> optionalDeviceEntity = this.deviceRepository.findById(device.getID());
+		if (optionalDeviceEntity.isPresent()) {
+			DeviceEntity deviceEntity = optionalDeviceEntity.get();
+			this.deviceRepository.save(DeviceEntity.builder()
+					.ID(device.getID())
+					.vendor(device.getVendor())
+					.isOnline(device.isOnline())
+					.gateway(deviceEntity.getGateway())
+					.createDate(deviceEntity.getCreateDate())
+					.build());
+		} else {
+			throw new NotFoundException(String.format("Device with ID %s not found", device.getID()));
+		}
+	}
+	
+	@DeleteMapping("/device/{id}")
+	public void removeDevice(@PathVariable("id") int ID) {
+		Optional<DeviceEntity> optionalDeviceEntity = this.deviceRepository.findById(ID);
+		if (optionalDeviceEntity.isPresent()) {
+			this.deviceRepository.deleteById(ID);
+		} else {
+			throw new NotFoundException(String.format("Device with ID %s not found", ID));
+		}
+	}
+	
 	@PutMapping(path = "/device")
 	public void addDevice(@RequestBody Device device) {
-		if (isEmpty(device.getGatewayID())) {
-			throw
+		
+		String gatewayID = device.getGatewayID();
+		if (isEmpty(gatewayID)) {
+			throw new InvalidArgumentException("The device must include a gateway UUID, create one if none available");
 		}
+		
+		Optional<GatewayEntity> gatewayOptional = this.gatewayRepository.findById(gatewayID);
+		if (!gatewayOptional.isPresent()) {
+			throw new NotFoundException("The provided gateway UUID isn't valid");
+		}
+		
+		GatewayEntity gatewayEntity = gatewayOptional.get();
+		if (gatewayEntity.getDeviceList().size() >= 10) {
+			throw new InvalidArgumentException("The gateway has reached it's device limit, consider removing some devices or create a new gateway");
+		}
+		
 		deviceRepository.save(DeviceEntity.builder()
 				.createDate(new Date())
-				.gateway(new GatewayEntity(device.getGatewayID()))
+				.gateway(new GatewayEntity(gatewayID))
 				.isOnline(device.isOnline())
 				.vendor(device.getVendor())
 				.build());
