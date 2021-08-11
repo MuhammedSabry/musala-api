f50abab34f2d7ed576d3821edcf125a054e965ea
MuhammedSabry <muhammed.sabry.refai@gmail.com>
2021-08-11 21:28:01 +0200

    Fixed cors stopping requests from client.

--- server/src/main/java/com/muhammed/sabry/musalaapi/config/SpringSecurityConfig.java
+++ server/src/main/java/com/muhammed/sabry/musalaapi/config/SpringSecurityConfig.java
@@ -3,14 +3,24 @@ package com.muhammed.sabry.musalaapi.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.csrf()
+				.disable()
+				.cors()
 				.disable()
 				.authorizeRequests()
 				.antMatchers("/device", "/device/**").permitAll()
